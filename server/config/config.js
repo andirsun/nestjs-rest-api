73914d8ce41b9b6cb7df62159f28fee2ff6d8b1b
Anderson Laverde <ander.laverde.dev@gmail.com>
2019-11-17 13:29:03 -0500

    first commit  with the first configuration

---
+++ server/config/config.js
@@ -0,0 +1,24 @@
+// ============================
+//  Puerto
+// ============================
+process.env.PORT = process.env.PORT || 3000;
+
+
+// ============================
+//  Entorno
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ============================
+//  Base de datos
+// ============================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = "mongodb+srv://admin:y8Rf@y8Rf@bnjiYKEk8_@timugo-d2l1g.mongodb.net/test"
+    //urlDB = process.env.MONGO_URI;
+}
+process.env.URLDB = urlDB;
