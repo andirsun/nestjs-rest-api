a4fbf045edb0a3164c42df76beb64a9fc735f2c2
Anderson Laverde <ander.laverde.dev@gmail.com>
2019-11-24 03:09:22 -0500

    sincro

--- server/server.js
+++ server/server.js
@@ -31,7 +31,10 @@ mongoose.connect(process.env.URLDB,
     console.log('Base de datos ONLINE');
 
 });
-
+process.on('uncaughtException', function (error) {
+    console.log(error.stack);
+    
+ });
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto: ', process.env.PORT);
 });
