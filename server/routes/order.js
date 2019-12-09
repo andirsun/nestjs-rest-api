f28b2f37223ac3722cc61af6d44a61faf297144f
Anderson Laverde <ander.laverde.dev@gmail.com>
2019-12-09 02:57:55 -0500

    small fix order

--- server/routes/order.js
+++ server/routes/order.js
@@ -9,55 +9,57 @@ const moment = require('moment');
 //const timezone = require('moment-timezone');
 
 
-app.post("/createOrder", function(req, res) {
-    ///Add user to DB the data is read by body of the petition
-    let body = req.body;
-    temporalOrder.find(function(err, temporalOrderDB) {
+app.post("/createOrder", function (req, res) {
+  ///Add user to DB the data is read by body of the petition
+  let body = req.body;
+  temporalOrder.find(function (err, temporalOrderDB) {
+    if (err) {
+      return res.status(400).json({
+        response: 1,
+        content: err
+      });
+    }
+    let id = temporalOrderDB.length + 1; //Autoincremental id
+    let idClient = body.idClient;
+    let idBarber = body.idBarber || 0;
+    let address = body.address;
+    let dateBeginOrder = moment().format("YYYY-MM-DD");
+    let hourStart = moment().format("HH:mm");
+    let typeService = body.typeService;
+    let status = body.status;
+    let order = new temporalOrder({
+      id,
+      idClient,
+      idBarber,
+      address,
+      dateBeginOrder,
+      typeService,
+      hourStart,
+      status,
+    });
+
+    order.save((err, orderDB) => {
+      //callback que trae error si no pudo grabar en la base de datos y usuarioDB si lo inserto
       if (err) {
-        return res.status(400).json({
+        return res.status(500).json({
           response: 1,
           content: err
         });
       }
-      let id = temporalOrderDB.length + 1; //Autoincremental id
-      let idClient = body.idClient;
-      let idBarber = body.idBarber || 0;
-      let address = body.address;
-      let dateBeginOrder = moment().format("YYYY-MM-DD");
-      let hourStart = moment().format("HH:mm");
-      let typeService = body.typeService;
-      let status = body.status;
-      let order = new temporalOrder({
-        id,
-        idClient,
-        idBarber,
-        address,
-        dateBeginOrder,
-        typeService,
-        hourStart,
-        status,
-      });
-  
-      order.save((err, orderDB) => {
-        //callback que trae error si no pudo grabar en la base de datos y usuarioDB si lo inserto
-        if (err) {
-          return res.status(500).json({
-            response: 1,
-            content: err
-          });
-        }
-        if(orderDB){
-            res.status(200).json({
-            response: 2,
-            content: {
-                orderDB,
-                message: "Temporal Order Created !!!"
-            }
-            });
-        }
-      });
+      if (orderDB) {
+        res.status(200).json({
+          response: 2,
+          content: {
+            orderDB,
+            message: "Temporal Order Created !!!"
+          }
+        });
+      }
     });
   });
+});
+
+
 
 
 
