716b1a8c0e3585be8a1d75ad734165f4aaa1b1c9
Anderson Laverde <ander.laverde.dev@gmail.com>
2019-12-09 02:27:34 -0500

    fix in createService and Add GetServices

--- server/routes/service.js
+++ server/routes/service.js
@@ -7,50 +7,66 @@ const moment = require('moment');
 //const timezone = require('moment-timezone');
 
 
-app.post("/createService", function(req, res) {
+app.post("/createService", function (req, res) {
     ///Add user to DB the data is read by body of the petition
     let body = req.body;
-    service.find(function(err, serviceDB) {
-      if (err) {
-        return res.status(500).json({
-          response: 1,
-          content: err
-        });
-      }
-      let id = seriviceDB.length + 1; //Autoincremental id
-      let name = body.name;
-      let price = body.price;
-      let description = body.description;
-      let urlImg = body.urlImg;
-      let serviceSave = new temporalOrder({
-        id,
-        name,
-        price,
-        description,
-        urlImg
-      });
-  
-      serviceSave.save((err, serviceSaveDB) => {
-        //callback que trae error si no pudo grabar en la base de datos y usuarioDB si lo inserto
+    service.find(function (err, serviceDB) {
         if (err) {
-          return res.status(500).json({
-            response: 1,
-            content: err
-          });
-        }
-        if(serviceSaveDB){
-            res.status(200).json({
-            response: 2,
-            content: {
-                serviceSaveDB,
-                message: "Service Created !!!"
-            }
+            return res.status(500).json({
+                response: 1,
+                content: err
             });
         }
-      });
-    });
-  });
+        let id = serviceDB.length + 1; //Autoincremental id
+        let name = body.name;
+        let price = body.price;
+        let description = body.description;
+        let urlImg = body.urlImg;
+        let serviceSave = new service({
+            id,
+            name,
+            price,
+            description,
+            urlImg
+        });
 
+        serviceSave.save((err, serviceSaveDB) => {
+            //callback que trae error si no pudo grabar en la base de datos y usuarioDB si lo inserto
+            if (err) {
+                return res.status(500).json({
+                    response: 1,
+                    content: err
+                });
+            }
+            if (serviceSaveDB) {
+                res.status(200).json({
+                    response: 2,
+                    content: {
+                        serviceSaveDB,
+                        message: "Service Created !!!"
+                    }
+                });
+            }
+        });
+    });
+});
+
+app.get("/getServices",function(req,res){
+    service.find(function(err,serviceDB){
+        if (err) {
+            return res.status(500).json({
+                response: 1,
+                content: err
+            });
+        }
+        if(serviceDB){
+            res.status(200).json({
+                response: 2,
+                content: serviceDB
+            });
+        }
+    });
+});
 
 
 
