6386a02e8def9a8b41577d521a0bd1f51d6ea9df
Anderson Laverde <ander.laverde.dev@gmail.com>
2019-12-10 17:29:25 -0500

    get current order done

--- server/routes/order.js
+++ server/routes/order.js
@@ -9,12 +9,42 @@ const moment = require('moment');
 //const timezone = require('moment-timezone');
 
 
+app.get("/getCurrentOrder",function(req,res){
+  let body = req.body;
+  let idClient = parseInt(body.id);
+  
+  temporalOrder.findOne({idClient:idClient},function(err,temporalOrderDB){
+    if (err) {
+      return res.status(500).json({
+        response: 1,
+        content: err
+      });
+    }
+    //Here i need to conslut the information aboout the barber and build a json with all information to return
+    if(temporalOrderDB){
+      res.status(200).json({
+        response: 2,
+        content: {
+          order:temporalOrderDB,
+          barber: "Aqui va la informacion del barbero"
+        }
+      });
+    }else{
+      res.status(200).json({
+        response: 1,
+        content: {
+          message: "No tienes ninguna order en proceso."
+        }
+      });
+    }
+  });
+});
 app.post("/createOrder", function (req, res) {
   ///Add user to DB the data is read by body of the petition
   let body = req.body;
   temporalOrder.find(function (err, temporalOrderDB) {
     if (err) {
-      return res.status(400).json({
+      return res.status(500).json({
         response: 1,
         content: err
       });
