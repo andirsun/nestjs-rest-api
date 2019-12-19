ba755bd48399b02fd901cdb088641ed48dd0e337
Anderson Laverde <ander.laverde.dev@gmail.com>
2019-12-18 21:00:07 -0500

    add login barber

--- server/routes/barber.js
+++ server/routes/barber.js
@@ -4,7 +4,61 @@ const _ = require("underscore");
 const Barber = require("../models/barber");
 const jwt = require("jsonwebtoken");
 const app = express();
+const temporalOrder = require("../models/temporalOrder");
+
 /////////////////////////////////
+
+app.post("/loginBarber" ,function(req,res){
+  
+  let body = _.pick(req.body, ["phone"]);
+  //let body = req.query;
+  Barber.findOne({phone: body.phone},function(err, user) {
+      if (err) {
+        return res.status(500).json({
+          response: 3,
+          content: {
+            error: err,
+            message: "Error al buscar el barbero"
+          }
+        });
+      }
+      if (user) {
+        //in case that the barber exists in de data base
+        let barber = user.toJSON(); //handling theresponse
+        temporalOrder.findOne({idBarber:barber.id},function(err,response){
+          if (err) {
+            return res.status(500).json({
+              response: 3,
+              content: {
+                error: err,
+                message: "Error al buscar si un barbero tiene un pedido en curso"
+              }
+            });
+          }
+          if(response){
+            res.status(200).json({
+              response: 2,
+              content:"Barbero logeado, pero con pedido en curso"
+            });    
+          }else{
+            res.status(200).json({
+              response: 2,
+              content:"Barbero logeado correctamente"
+            });
+          }
+        });
+       
+      } else {
+        res.json({
+          response: 1,
+          content:
+            "Ups, no encontramos ningun barbero con ese Celular"
+        });
+      }
+    }
+  );
+
+});
 app.post("/addBarber", function(req, res) {
   ///Add user to DB the data is read by body of the petition
   let body = req.body;
@@ -22,6 +76,7 @@ app.post("/addBarber", function(req, res) {
     let email = body.email;
     let birth = body.birth;
     let phone = body.phone;
+    let city = body.city;
     let pass = bcrypt.hashSync(body.pass, 10);
     let document = body.document;
     let bio = body.bio || "";
@@ -34,6 +89,7 @@ app.post("/addBarber", function(req, res) {
       birth: birth,
       phone: phone,
       password: pass,
+      city,
       document: document,
       bio: bio
     });
