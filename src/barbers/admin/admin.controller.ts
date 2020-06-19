7f6c451bd28f4dfefd2988d8c6545d48757fdb21
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2020-06-18 23:12:46 -0500

    feat(Admin): Get Emails from users

--- src/barbers/admin/admin.controller.ts
+++ src/barbers/admin/admin.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+// Nest dependencies
+import { Controller, Get, Res, HttpStatus } from '@nestjs/common';
+// External services
+import { UserService } from "../user/user.service";
+
 
 @Controller('admin')
-export class AdminController {}
+export class AdminController {
+
+  constructor(
+    private userService : UserService,
+  ){}
+  /*
+    This endpoint return all barbers in a city
+  */
+  @Get('/barbers/users/emails')
+  async getEmailUsers(@Res() res){
+    this.userService.getUserEmails()
+      .then((emails)=>{
+        let emailsArray : [string] = [""]
+
+        let cycle = new Promise((resolve, reject) => {
+          emails.forEach((value, index, array) => {
+              emailsArray.push(value.email);
+              if (index === array.length -1) resolve();
+          });
+        });
+      
+        cycle.then(() => {
+          return res.status(HttpStatus.OK).json({
+            response: 2,
+            content: {
+              emails
+            }
+          });
+        });
+      })
+      .catch((err)=>{
+        res.status(HttpStatus.BAD_REQUEST).json({
+          response: 3,
+          content: err
+        });
+        throw new Error(err);
+      });   
+  }
+}
--- src/barbers/admin/admin.module.ts
+++ src/barbers/admin/admin.module.ts
@@ -1,9 +1,16 @@
+// Nest dependencies
 import { Module } from '@nestjs/common';
+// services
 import { AdminService } from './admin.service';
+// Controllers
 import { AdminController } from './admin.controller';
+// Modules
+import { UserModule } from '../user/user.module';
 
 @Module({
+  imports : [UserModule],
   providers: [AdminService],
   controllers: [AdminController]
+  
 })
 export class AdminBarbersModule {}
