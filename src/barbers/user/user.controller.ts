363a83d1b8d114623f1027d01558484237b9e453
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2020-06-30 01:05:18 -0500

    feat(User): new endpoint migrated

--- src/barbers/user/user.controller.ts
+++ src/barbers/user/user.controller.ts
@@ -1,5 +1,5 @@
 /* Nest Js dependencies */
-import { Controller,Get,Post,Put,Delete,Res,HttpStatus,Body, Query} from '@nestjs/common';
+import { Controller,Get,Post,Put,Delete,Res,HttpStatus,Body, Query, Inject, forwardRef} from '@nestjs/common';
 /*
     Data Onjects Transfer are all the interfaces to transfer betwen this class en requests
 */
@@ -12,86 +12,125 @@ import { LogBarbersService } from "../log-barbers/log-barbers.service";
 import { PromotionalCodeService } from '../promotional-codes/promotional-codes.service';
 import { TimeService } from '../time/time.service';
 import { UserPromCodeInterface } from './interfaces/user-promcode.interface';
+import { OrdersService } from '../orders/orders.service';
 
 
 @Controller('user')
 export class UserController {
 
-	constructor(
-		private userService : UserService,
+  constructor(
+    private userService : UserService,
     private logService : LogBarbersService,
     private promotionalCodeService: PromotionalCodeService,
-    private timeService: TimeService
+    private timeService: TimeService,
+    private ordersService : OrdersService
 
-	){}
-	
-	/*
-		This endpoint return a specific user
-		searching by id
-	*/
-	@Get('/info')
-	async getUserById(@Res() res,@Query('idUser')idUser : number){
-		await this.userService.getUser(idUser)
-			.then(user=>{
-				
-				return res.status(HttpStatus.OK).json({
-					response: 2,
-					content:{
-						user
-					}
-				});
-			})
-			.catch(err=>{
-				/* Sentry report */
-				res.status(HttpStatus.BAD_REQUEST).json({
-					response: 1,
-					content:{
-						message : "no encontramos a un usuario con ese id"
-					}
-				});
-				throw new Error(err);
-			})
-	}
-	/*
-		This endpoint return all users
-	*/
-	@Get('/barbers/getAll')
-	async getUsers(@Res() res){
-		await this.userService.getUsers()
-			.then(users=>{
-				this.logService.log("Se consultaron los usuarios","none");
-				return res.status(HttpStatus.OK).json({
-					response: 2,
-					content:{
-						users
-					}
-				});
-			})
-			.catch(err=>{
-				/* Local log */
-				this.logService.error("Error al consultar los usuarios","none");
-				/* Sentry report */
-				throw new Error(err);
-			})
-	}
-	//This endopoint can be accesed with url/user/createNewUser
-	@Post('/createUser')
-	async createUser(@Res() res, @Body() createUserDTO : CreateUserDTO){
-			await this.userService.createUser(createUserDTO)
-					.then((user)=>{
-							console.log("Llegue positivo");
-							return res.status(HttpStatus.OK).json({
-									response: 2,
-									content: user
-							});
-					})
-					.catch((err)=>{
-							console.log("llegue negativo");
-							return res.status(HttpStatus.BAD_REQUEST).json({
-									response: 3,
-									content: err
-							});
-					});   
+  ){}
+  
+  /*
+    This endpoint return a specific user
+    searching by id
+  */
+  @Get('/info')
+  async getUserById(@Res() res,@Query('idUser')idUser : number){
+    await this.userService.getUser(idUser)
+      .then(user=>{
+        return res.status(HttpStatus.OK).json({
+          response: 2,
+          content:{
+            user
+          }
+        });
+      })
+      .catch(err=>{
+        /* Sentry report */
+        res.status(HttpStatus.BAD_REQUEST).json({
+          response: 1,
+          content:{
+            message : "no encontramos a un usuario con ese id"
+          }
+        });
+        throw new Error(err);
+      })
+  }
+  /*
+    This endpoint return all users of Timugo Barbers
+  */
+  @Get('/getAll')
+  async getUsers(@Res() res){
+    await this.userService.getUsers()
+      .then(users=>{
+        this.logService.log("Se consultaron los usuarios","none");
+        return res.status(HttpStatus.OK).json({
+          response: 2,
+          content:{
+            users
+          }
+        });
+      })
+      .catch(err=>{
+        /* Local log */
+        this.logService.error("Error al consultar los usuarios","none");
+        /* Sentry report */
+        throw new Error(err);
+      })
+  }
+  /*
+    this function check if an user is in a order in progress
+  */
+  @Get('/checkUserOrder')
+  async checkUserInCurrentOrder (@Res() res, @Query('idUser')idUser : string){
+    this.ordersService.getActiveOrdersByIdUserAndStatus(idUser,"PENDING")
+      .then(order=>{
+        if (!order) {
+          return res.status(HttpStatus.FORBIDDEN).json({
+            response: 1,
+            content: {
+              message : "El usuario no tiene ninguna orden pendiente"
+            }
+          });  
+        } else {
+          return res.status(HttpStatus.OK).json({
+            response: 1,
+            content: {
+              message : "El usuario no tiene ninguna orden pendiente"
+            }
+          });
+        }
+      })
+      .catch(err =>{
+        res.status(HttpStatus.BAD_REQUEST).json({
+          response: 3,
+          content: {
+            error : err
+          }
+        });
+        throw new Error(err);
+      })
+  }
+  /*
+    This endpoint create an user
+  */
+  @Post('/createUser')
+  async createUser(@Res() res, @Body() createUserDTO : CreateUserDTO){
+    await this.userService.createUser(createUserDTO)
+      .then((user)=>{
+        return res.status(HttpStatus.OK).json({
+          response: 2,
+          content: {
+            user
+          }
+        });
+      })
+      .catch((err)=>{
+        res.status(HttpStatus.BAD_REQUEST).json({
+          response: 3,
+          content: {
+            error : err
+          }
+        });
+        throw new Error(err);
+      });   
   }
   
   @Post('/linkPromCode')
