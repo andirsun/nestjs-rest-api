f3822bc432691f7418b55ad1256ad528794bc8af
davidAI04 <david.algorithm@hotmail.com>
2020-07-02 17:02:09 -0500

    fix() duplicated endpoints

--- src/barbers/user/user.controller.ts
+++ src/barbers/user/user.controller.ts
@@ -23,57 +23,6 @@ export class UserController {
     private timeService: TimeService,
     private ordersService : OrdersService
   ){}
-	
-	/*
-		This endpoint return a specific user
-		searching by id
-	*/
-	@Get('/info')
-	async getUserById(@Res() res,@Query('idUser')idUser : number){
-		await this.userService.getUser(idUser)
-			.then(user=>{
-				
-				return res.status(HttpStatus.OK).json({
-					response: 2,
-					content:{
-						user
-					}
-				});
-			})
-			.catch(err=>{
-				/* Sentry report */
-				res.status(HttpStatus.BAD_REQUEST).json({
-					response: 1,
-					content:{
-						message : "no encontramos a un usuario con ese id"
-					}
-				});
-				throw new Error(err);
-			})
-	}
-	/*
-		This endpoint return all users
-	*/
-	@Get('/barbers/getAll')
-	async getUsers(@Res() res){
-		await this.userService.getUsers()
-			.then(users=>{
-			  this.logService.log("Se consultaron los usuarios","none");
-				return res.status(HttpStatus.OK).json({
-					response: 2,
-					content:{
-						users
-					}
-				});
-			})
-			.catch(err=>{
-				/* Local log */
-				this.logService.error("Error al consultar los usuarios","none");
-				/* Sentry report */
-				throw new Error(err);
-			})
-  }
-  
   /*
 		This endpoint return all valid user's prom codes
 	*/
@@ -132,26 +81,6 @@ export class UserController {
       })
   }
 
-
-	//This endopoint can be accesed with url/user/createNewUser
-	@Post('/createUser')
-	async createUser(@Res() res, @Body() createUserDTO : CreateUserDTO){
-			await this.userService.createUser(createUserDTO)
-					.then((user)=>{
-							console.log("Llegue positivo");
-							return res.status(HttpStatus.OK).json({
-									response: 2,
-									content: user
-							});
-					})
-					.catch((err)=>{
-							console.log("llegue negativo");
-							return res.status(HttpStatus.BAD_REQUEST).json({
-									response: 3,
-									content: err
-							});
-					});   
-  }
   /*
     This endpoint return a specific user
     searching by id
