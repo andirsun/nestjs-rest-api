87b4a8c0a2075562c2fd9911685b77c11c54db91
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2020-07-08 11:43:25 -0500

    chore(): remove unused properties in dto

--- src/barbers/user/domain/dto/user.dto.ts
+++ src/barbers/user/domain/dto/user.dto.ts
@@ -1,15 +1,5 @@
 export class CreateUserDTO {
-    readonly id: number;
-    readonly phoneToken:string;
-    readonly updated: string;
     readonly name: string;
-    readonly registrationCode :string;
     readonly phone: string;
-    readonly birth: string;
     readonly email: string;
-    readonly img: string;
-    readonly numServices:string;
-    readonly points: number;
-    readonly status: boolean;
-    readonly publicityMethod:string
 }
