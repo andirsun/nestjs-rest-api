5dbe261554565eca9d6c2dbfac728293a8f1ec06
Anderson Laverde Gracia <ander.laverde.dev@gmail.com>
2020-07-08 19:41:10 -0500

    feat(Port): set default port by .env file

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,5 @@
 require("dotenv").config();
 // ============================
-//  Port BY Default
-// ============================
-process.env.PORT = process.env.PORT || 3000;
-
-// ============================
 //  Enviroment
 // ============================
 process.env.NODE_ENV = process.env.NODE_ENV || "dev";
